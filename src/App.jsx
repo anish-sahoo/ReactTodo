83f3f0ae54566f6e75eeb161b52b04a0a005639d
Anish Sahoo <anishsahoo2005@gmail.com>
2023-07-20 15:11:34 -0500

    added remove button, not fully working yet.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import TodoList from './TodoList';
 import TabList from './TabList';
 import './stylesheets/App.css';
+import { render } from 'react-dom';
 
 const App = () => {
   const [todoList, setTodoList] = useState([]);
@@ -17,11 +18,19 @@ const App = () => {
 
   const handleTodoClick = (index) => {
     const todo = todoList.find(el => el.index === index);
-
+    if(todoList.every(element => element.done === true)){
+      console.log('Add confetti here');
+    }
     if (todo) {
       todo.done = !todo.done;
       setTodoList([...todoList]);
     }
+  }
+
+  const handleDeleteClick = (index) => {
+    console.log(`Element ${todoList[index-1]} at ${index-1} removed.`);
+    todoList.splice(index-1, 1);
+    //setTodoList([...todoList]);
   }
 
   return (
@@ -36,16 +45,11 @@ const App = () => {
         onKeyPress={(e) => handleEnterPress(e)}
       />
 
-      <TabList />
+      <TabList/>
 
       {todoList.length > 0 ? (
-        <TodoList todoList={todoList} handleTodoClick={handleTodoClick} />
-      ) : (
-        <div className="blank-well">
-          No todo found
-        </div>
-      )}
-
+        <TodoList todoList={todoList} handleTodoClick={handleTodoClick} handleDeleteClick={handleDeleteClick} />
+      ) : ( <div className="blank-well">No todo found</div> )}
     </div>
   );
 }
