037754ea63c226963f6e5824cb78bb144eb0e3af
Anish Sahoo <anishsahoo2005@gmail.com>
2023-07-24 16:27:57 -0500

    added tab code

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import './stylesheets/App.css';
 
 const App = () => {
   const [todoList, setTodoList] = useState([]);
+  const [displayTodoList, setDisplayList] = useState([]);
   const [text, setText] = useState('');
+  
+  const tabs_list = [ {text:'all', enabled:true}, 
+                      {text:'undone', enabled:false}, 
+                      {text:'done', enabled:false} ]
+  const [tabList, setTabList] = useState(tabs_list);
 
   const handleEnterPress = (e) => {
     if (e.key === 'Enter') {
@@ -34,6 +40,17 @@ const App = () => {
     );
   }
 
+  const handleTabSelect = (value) => {
+    const tab = tabList.find(el => el.text === value);
+    console.log(tab);
+    if(tab.enabled){
+
+    }
+    else {
+
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="header">My Todo List</h1>
@@ -46,7 +63,7 @@ const App = () => {
         onKeyPress={(e) => handleEnterPress(e)}
       />
 
-      <TabList/>
+      <TabList handleClick={handleTabSelect}/>
 
       {todoList.length > 0 ? (
         <TodoList todoList={todoList} handleTodoClick={handleTodoClick} handleDeleteClick={handleDeleteClick} />
--- src/TabList.jsx
+++ src/TabList.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Tab from './Tab';
 
-const TabList = () => {
-  const tabs = ['all', 'undone', 'done']
-
+const TabList = ({tabList, handleClick}) => {
+  const tabs = [{text:'all', enabled:true}, {text:'undone', enabled:false}, {text:'done', enabled:false}];
+  
   return (
     <div className="tab-list-container">
-      {tabs.map(tab => <Tab key={tab} value={tab} />)}
+      {tabs.map(tab => <Tab 
+        key={tabs.indexOf(tab)} 
+        value={tab.text}
+        enabled={tab.enabled}
+        handleClick={handleClick} />)}
     </div>
   );
 }
